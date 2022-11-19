85033d51c7240a8a5f40b724c082f584923e1e42
Tushar Singh <tusharsingh.contact@gmail.com>
2022-11-19 19:08:25 +0530

    Update ProblemStatement5.sql

--- DBMS/ProblemStatement5.sql
+++ DBMS/ProblemStatement5.sql
@@ -5,7 +5,7 @@ CREATE TABLE Customers (
 CREATE TABLE Orders (
 	order_id INT,
 	amount INT,
-	cusotmer_id INT
+	customer_id INT
 );
 
 INSERT INTO Customers VALUES (1, 'John');
@@ -19,3 +19,11 @@ INSERT INTO Orders VALUES (2, 300, 4);
 INSERT INTO Orders VALUES (3, 12000, 3);
 INSERT INTO Orders VALUES (4, 400, 1);
 INSERT INTO Orders VALUES (5, 250, 2);
+
+-- NATURAL INNER JOIN
+
+SELECT Customers.customer_id, first_name, order_id, amount 
+	FROM Customers
+    INNER JOIN Orders 
+    ON Customers.customer_id = Orders.customer_id; 
+
