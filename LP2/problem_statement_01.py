1ea1616f9ca74bb306bc41321e66cbc2f1541080
Tushar Singh <tusharsingh.contact@gmail.com>
2023-05-26 00:51:14 +0530

    Update problem_statement_01.py

--- LP2/problem_statement_01.py
+++ LP2/problem_statement_01.py
@@ -1,19 +1,18 @@
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-
-    print(start, end=' ')
-
-    for next in graph[start] - visited:
-        dfs(graph, next, visited)
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
     return visited
 
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
 
-graph = {'0': set(['1', '2']),
-         '1': set(['0', '3', '4']),
-         '2': set(['0']),
-         '3': set(['1']),
-         '4': set(['2', '3'])}
-
-dfs(graph, '0')
+print(dfs(graph, 'A'))
